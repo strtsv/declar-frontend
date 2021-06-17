7417b2a6bc6b594e66652ba13299d5bb46ed083b
jsusilo <enr@bk.ru>
2021-06-17 15:12:26 +0500

    shedule page

---
+++ src/pages/Shedule.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Header";
+import OurDJs from "../components/Shedule/OurDJs";
+import Footer from "../components/Footer";
+
+const Shedule = () => {
+  return (
+    <div className="page animated">
+      <Header />
+      <OurDJs />
+      <Footer />
+    </div>
+  );
+};
+
+export default Shedule;
